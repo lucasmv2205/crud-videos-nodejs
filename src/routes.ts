dad55eb9ce1e6cce5a8ac2971d4b4696e6f37d5a
lucasmv2205 <lmartins2205@gmail.com>
2022-08-18 15:20:51 -0300

    start crud video

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateCategoryController } from './controllers/CreateCategoryController
 import { GetAllCategoriesController } from './controllers/GetAllCategoriesController';
 import { DeleteCategoriesController } from './controllers/DeleteCategoriesController';
 import { UpdateCategoryController } from './controllers/UpdateCategoryController';
+import { CreateVideoController } from './controllers/CreateVideoController';
 
 const routes = Router();
 
@@ -10,5 +11,8 @@ routes.post("/categories", new CreateCategoryController().handle);
 routes.get("/categories", new GetAllCategoriesController().handle);
 routes.delete("/categories/:id", new DeleteCategoriesController().handle);
 routes.put("/categories/:id", new UpdateCategoryController().handle);
+
+
+routes.post("/videos", new CreateVideoController().handle);
 
 export { routes }
